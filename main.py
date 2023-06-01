61054cba8a2d1abdbc27e917e1c4a7fc7761a407
Ryder Retzlaff <retzerrt@gmail.com>
2023-06-01 16:10:36 +1000

    Better Styling

--- main.py
+++ main.py
@@ -5,38 +5,44 @@ import requests
 
 app = Flask(__name__)
 
+
 def get_filename_from_url(url):
-    
     parsed_url = urllib.parse.urlparse(url)
     try:
         filename = parsed_url.path
     except:
-        # Si falla es porque la implementación de parsed_url no reconoce los atributos como "path"
-        if len(parsed_url)>=4:
+        if len(parsed_url) >= 4:
             filename = parsed_url[2]
         else:
             filename = ""
 
-    if len(filename)>0:
-
+    if len(filename) > 0:
         # Remove trailing slash
-        if filename[-1:]=="/":
+        if filename[-1:] == "/":
             filename = filename[:-1]
-        
+
         if "/" in filename:
             filename = filename.split("/")[-1]
 
-    return filename 
+    return filename
+
 
 @app.route("/")
 def home():
     return render_template("download.html")
+
 
 @app.route("/download/")
 def download():
     r = requests.get(request.args.get("url"))
     b = BytesIO(r.content)
     b.name = get_filename_from_url(request.args.get("url"))
-    return send_file(b, mimetype=r.headers['Content-Type'].split(";")[0], as_attachment=True, download_name=get_filename_from_url(request.args.get("url")))
+    return send_file(
+        b,
+        mimetype=r.headers["Content-Type"].split(";")[0],
+        as_attachment=True,
+        download_name=b.name,
+    )
 
-app.run("0.0.0.0", 80)
+
+app.run("0.0.0.0", 80, debug=True)
